24250290263796a6a556c198bea56836625dae8c
DSC <2233429841@qq.com>
2024-09-10 15:30:08 +0800

    upload '9_RegNet' folder

---
+++ Project/项目实战/pytorch_classification/9_RegNet/README.md
@@ -0,0 +1,16 @@
+## 代码使用简介
+
+1. 下载好数据集，代码中默认使用的是花分类数据集，下载地址: [https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz](https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz)
+
+2. 在`train.py`脚本中将`--data-path`设置成解压后的`flower_photos`文件夹绝对路径。
+
+3. 下载预训练权重，根据自己使用的模型下载对应预训练权重: https://pan.baidu.com/s/17H23Hiilz6PP37MMhOxWdw ，
+提取码：cpdd
+
+4. 在`train.py`脚本中将`--weights`参数设成下载好的预训练权重路径。
+
+5. 在`predict.py`脚本中导入和训练脚本中同样的模型，并将`model_weight_path`设置成训练好的模型权重路径(默认保存在weights文件夹下)。
+
+6. 在`predict.py`脚本中将`img_path`设置成你自己需要预测的图片绝对路径。
+
+7. 如果要使用自己的数据集，请按照花分类数据集的文件结构进行划分(即一个类别对应一个文件夹)，并且将训练以及预测脚本中的`num_classes`设置成你自己数据的类别数。
---
+++ Project/项目实战/pytorch_classification/9_RegNet/train.py
@@ -0,0 +1,182 @@
+import os
+import math
+import argparse
+import json
+
+import torch
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms, datasets
+import torch.optim.lr_scheduler as lr_scheduler
+
+from model import create_regnet
+from utils import train_one_epoch, evaluate
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    print(args)
+    print(
+        'Start Tensorboard with "tensorboard --logdir=runs", view at http://localhost:6006/'
+    )
+    tb_writer = SummaryWriter()
+    if os.path.exists("./weights") is False:
+        os.makedirs("./weights")
+
+    data_transform = {
+        "train": transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+        "val": transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+    }
+
+    # get data root path
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "./.."))
+    # flower data set path
+    image_path = os.path.join(data_root, "data_set", "flower_data")
+
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(
+        root=os.path.join(image_path, "train"), transform=data_transform["train"]
+    )
+    train_num = len(train_dataset)
+
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open("class_indices.json", "w") as json_file:
+        json_file.write(json_str)
+
+    batch_size = args.batch_size
+    nw = min(
+        [os.cpu_count(), batch_size if batch_size > 1 else 0, 8]
+    )  # number of workers
+    print("Using {} dataloader workers every process".format(nw))
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=nw,
+        pin_memory=True,  # 是否将数据固定在CPU而不复制到GPU
+        prefetch_factor=2,  # 预加载多少个epoch的数据
+    )
+
+    validate_dataset = datasets.ImageFolder(
+        root=os.path.join(image_path, "val"), transform=data_transform["val"]
+    )
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(
+        validate_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=nw,
+        pin_memory=True,
+        prefetch_factor=2,
+    )
+
+    print(
+        "using {} images for training, {} images for validation.".format(
+            train_num, val_num
+        )
+    )
+
+    # 如果存在预训练权重则载入
+    model = create_regnet(model_name=args.model_name, num_classes=args.num_classes).to(
+        device
+    )
+
+    if args.weights != "":
+        if os.path.exists(args.weights):
+            weights_dict = torch.load(args.weights, map_location=device)
+            load_weights_dict = {
+                k: v
+                for k, v in weights_dict.items()
+                if model.state_dict()[k].numel() == v.numel()
+            }
+            print(model.load_state_dict(load_weights_dict, strict=False))
+        else:
+            raise FileNotFoundError("not found weights file: {}".format(args.weights))
+
+    # 是否冻结权重
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除最后的全连接层外，其他权重全部冻结
+            if "head" not in name:
+                para.requires_grad_(False)
+            else:
+                print("train {}".format(name))
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=5e-5)
+    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
+    lf = (
+        lambda x: ((1 + math.cos(x * math.pi / args.epoches)) / 2) * (1 - args.lrf)
+        + args.lrf
+    )  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+
+    for epoch in range(1, args.epoches + 1):
+        # train
+        mean_loss = train_one_epoch(
+            model=model,
+            optimizer=optimizer,
+            data_loader=train_loader,
+            device=device,
+            epoch=epoch,
+        )
+
+        scheduler.step()
+
+        # validate
+        acc = evaluate(model=model, data_loader=validate_loader, device=device)
+
+        print("[epoch {}] accuracy: {}".format(epoch, round(acc, 3)))
+        tags = ["loss", "accuracy", "learning_rate"]
+        tb_writer.add_scalar(tags[0], mean_loss, epoch)
+        tb_writer.add_scalar(tags[1], acc, epoch)
+        tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch)
+        if epoch % (args.epoches / 2) == 0:
+            torch.save(model.state_dict(), "./weights/model-{}.pth".format(epoch))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_classes", type=int, default=5)
+    parser.add_argument("--epoches", type=int, default=4)
+    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--lrf", type=float, default=0.01)
+
+    parser.add_argument(
+        "--model-name", default="RegNetY_400MF", help="create model name"
+    )
+
+    # 预训练权重下载地址
+    # 链接：https://pan.baidu.com/s/1I1W9WmrFhy-ivWlkBzriSA 提取码：cpdd
+    parser.add_argument(
+        "--weights", type=str, default="regnety_400mf.pth", help="initial weights path"
+    )
+    parser.add_argument("--freeze-layers", type=bool, default=False)
+    parser.add_argument(
+        "--device", default="cuda:0", help="device id (i.e. 0 or 0,1 or cpu)"
+    )
+
+    opt = parser.parse_args()
+
+    main(opt)
