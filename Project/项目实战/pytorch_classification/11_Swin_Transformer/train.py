fc501c8e9aa3ef5720de4e4c6bc1d313c629caf5
DSC <2233429841@qq.com>
2024-10-15 16:20:59 +0800

    upload 11_Swin_Transformer folder

---
+++ Project/项目实战/pytorch_classification/11_Swin_Transformer/train.py
@@ -0,0 +1,170 @@
+import os
+import argparse
+import json
+
+import torch
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms, datasets
+
+from my_dataset import MyDataSet
+from model import swin_tiny_patch4_window7_224 as create_model
+from utils import read_split_data, train_one_epoch, evaluate
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    if os.path.exists("./weights") is False:
+        os.makedirs("./weights")
+
+    tb_writer = SummaryWriter()
+
+    train_images_path, train_images_label, val_images_path, val_images_label = (
+        read_split_data(args.data_path)
+    )
+
+    img_size = 224
+    data_transform = {
+        "train": transforms.Compose(
+            [
+                transforms.RandomResizedCrop(img_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+        "val": transforms.Compose(
+            [
+                transforms.Resize(int(img_size * 1.143)),
+                transforms.CenterCrop(img_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        ),
+    }
+
+    # get data root path
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "./.."))
+    # flower data set path
+    image_path = os.path.join(data_root, "data_set", "flower_data")
+
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(
+        root=os.path.join(image_path, "train"), transform=data_transform["train"]
+    )
+    train_num = len(train_dataset)
+
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open("class_indices.json", "w") as json_file:
+        json_file.write(json_str)
+
+    batch_size = args.batch_size
+    nw = min(
+        [os.cpu_count(), batch_size if batch_size > 1 else 0, 8]
+    )  # number of workers
+    print("Using {} dataloader workers every process".format(nw))
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=nw,
+        collate_fn=train_dataset.collate_fn,
+    )
+
+    val_dataset = datasets.ImageFolder(
+        root=os.path.join(image_path, "val"), transform=data_transform["val"]
+    )
+    val_num = len(val_dataset)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=nw,
+        pin_memory=True,
+        prefetch_factor=2,
+    )
+
+    print(
+        "using {} images for training, {} images for validation.".format(
+            train_num, val_num
+        )
+    )
+
+    model = create_model(num_classes=args.num_classes).to(device)
+
+    if args.weights != "":
+        assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(
+            args.weights
+        )
+        weights_dict = torch.load(args.weights, map_location=device)["model"]
+        # 删除有关分类类别的权重
+        for k in list(weights_dict.keys()):
+            if "head" in k:
+                del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除head外，其他权重全部冻结
+            if "head" not in name:
+                para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.AdamW(pg, lr=args.lr, weight_decay=5e-2)
+
+    for epoch in range(args.epochs):
+        # train
+        train_loss, train_acc = train_one_epoch(
+            model=model,
+            optimizer=optimizer,
+            data_loader=train_loader,
+            device=device,
+            epoch=epoch,
+        )
+
+        # validate
+        val_loss, val_acc = evaluate(
+            model=model, data_loader=val_loader, device=device, epoch=epoch
+        )
+
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
+        if epoch % (args.epoches / 2) == 0:
+            torch.save(model.state_dict(), "./weights/model-{}.pth".format(epoch))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_classes", type=int, default=5)
+    parser.add_argument("--epochs", type=int, default=4)
+    parser.add_argument("--batch-size", type=int, default=8)
+    parser.add_argument("--lr", type=float, default=0.0001)
+
+    # 预训练权重路径，如果不想载入就设置为空字符
+    parser.add_argument(
+        "--weights",
+        type=str,
+        default="./swin_tiny_patch4_window7_224.pth",
+        help="initial weights path",
+    )
+    # 是否冻结权重
+    parser.add_argument("--freeze-layers", type=bool, default=False)
+    parser.add_argument(
+        "--device", default="cuda:0", help="device id (i.e. 0 or 0,1 or cpu)"
+    )
+
+    opt = parser.parse_args()
+
+    main(opt)
